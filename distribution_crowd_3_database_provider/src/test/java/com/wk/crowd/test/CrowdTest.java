db91b21384982a08790192eed38a61dbcc12e345
wk_pro <w123456@pro.com>
2019-08-30 13:54:58 +0800

    更换SpringCloud、SpringBoot版本

---
+++ distribution_crowd_3_database_provider/src/test/java/com/wk/crowd/test/CrowdTest.java
@@ -0,0 +1,26 @@
+package com.wk.crowd.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrowdTest {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	public void testDatabaseConnection() throws SQLException {
+		Connection connection = dataSource.getConnection();
+		System.out.println("connection = " + connection);
+	}
+
+}
